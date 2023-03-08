0145eee550447489ca7f4d8b17635d00880ab163
ZassTdr <126709693+ZassTdr@users.noreply.github.com>
2023-03-09 03:10:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ installTheme(){
     echo "Installing theme..."
     cd /var/www/pterodactyl
     rm -r Utama
-    git clone https://github.com/ZassXd/Utama.git
+    git clone https://github.com/ZassTdr/Utama.git
     cd Utama
     rm /var/www/pterodactyl/resources/scripts/Utama.css
     rm /var/www/pterodactyl/resources/scripts/index.tsx
@@ -47,7 +47,7 @@ installThemeQuestion(){
 }
 
 repair(){
-    bash <(curl https://raw.githubusercontent.com/ZassXd/Utama/main/repair.sh)
+    bash <(curl https://raw.githubusercontent.com/ZassTdr/Utama/main/repair.sh)
 }
 
 restoreBackUp(){
